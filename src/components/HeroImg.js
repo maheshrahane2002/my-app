d23910ac7f99bdfc44c11709c871d1e5e8030e72
technOrbit <maheshrahane65@gmail.com>
2024-03-28 19:41:07 +0530

    To change new background on Scroll.

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import "./HeroImgStyles.css";
 import React from 'react';
 import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
@@ -8,9 +9,22 @@ const HeroImg = () => {
         <div className="mask">
         <img className="into-img" src={IntroImg} alt="IntroImg"/>
     </div>
+
+    <div className="content">
+    <p>HI,I'M A FREELANCER.</p>
+    <h1>React Developer.</h1>
+
+<div>
+  <Link to="/project" className="btn">Projects</Link>
+  <Link to="/contact" className="btn">Contact</Link>
+
+</div>
     </div>
+    </div>
+  
+    
     
   );
-};
+}; 
 
 export default HeroImg;
