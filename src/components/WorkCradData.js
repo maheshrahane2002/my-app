7e6ae5d3911dd8acffddc40141853d642b777795
technOrbit <maheshrahane65@gmail.com>
2024-03-29 17:32:16 +0530

    To create Dynamic card coponents.

---
+++ src/components/WorkCradData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "OCSMS",
+        text: "We are looking for an experienced Core PHP developer to build an Online Computer Services Management System.",
+        view: "http://localhost/ocsms_php/"
+    },
+    {
+        imgsrc: pro2,
+        title: "CMS",
+        text: "We are looking for an experienced Laravel developer to build an Cafe Management System.",
+        view: "http://localhost:8000"
+    },
+    {
+        imgsrc: pro3,
+        title: "MA",
+        text: "We are looking for an experienced mini-project developer to build an MovieApp.",
+        view: "http://localhost:3000"
+    },
+];
+export default ProjectCardData;
